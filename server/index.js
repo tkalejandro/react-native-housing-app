62fcbcd55a4d19b51933466072093fd236a08e45
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-03-02 17:46:52 +0100

    basic api setup added.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
+const bodyParser =  require('body-parser')
 const app = express()
+app.use(express.json());
+
 require('dotenv').config()
 
 const homes =[
@@ -16,20 +18,57 @@ const homes =[
 app.get('/', (req,res) => {
     res.send('Welcome to express')
 })
-app.get('/api/listening', (req, res) => {
+app.get('/api/listing', (req, res) => {
     res.send(homes)
 })
-app.get('/api/listening/:id', (req, res) => {
+app.get('/api/listing/:id', (req, res) => {
     const {id} = req.params
 
-    const home = homes.find(home => home.id === parseInt(req.params.id))
-
+    const home = homes.find(home => home.id === parseInt(id))
     if(!home) {
         res.status(404).send('The home with given ID is not found')
     }
 
     res.send(home)
    
+})
+
+app.post('/api/listing', (req, res) => {
+    if(!req.body.type || !req.body.description) return res.status(400).send("Title and description is required")
+    const home = {
+        id: homes.length + 1,
+        type: req.body.type,
+        description: req.body.description
+    }
+
+    homes.push(home)
+    res.send(home)
+})
+
+app.put('/api/listing/:id', (req, res) => {
+    const home = homes.find(home => home.id === parseInt(req.params.id))
+    
+    if(!home) {
+        res.status(404).send('The home with given ID is not found')
+    }
+
+    home.type = req.body.type
+    home.description = req.body.description
+   
+    res.send(home)
+   
+})
+
+app.delete('/api/listing/:id', (req, res) => {
+    const home = homes.find(home => home.id === parseInt(req.params.id))
+    
+    if(!home) {
+        res.status(404).send('The home with given ID is not found')
+    }
+
+    const index = homes.indexOf(home)
+    homes.splice(index, 1)
+    res.send(home)
 })
 
 const port = process.env.PORT || 3000
