92371abbaa9a1aa3eae3888df6fa8a7e5b15e92a
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-03-08 12:17:22 +0100

    Creating auth route and adding error validators.

--- auth-system/server/index.js
+++ auth-system/server/index.js
@@ -1,16 +1,27 @@
 import express from "express";
-
-
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import morgan from "morgan";
 //? Routes
 import auth from "./routes/auth.js"
 
 const app = express()
+dotenv.config()
 
-
+app.use(morgan("tiny"))
+app.use(express.json()); //? Body PArser
 app.get("/", (req, res) => {
     res.send("Welcome to the auth system")
 })
 
 app.use("/api/users", auth)
 
-app.listen(3000, () => console.log("Server is running"))
+const port = process.env.PORT || 3000
+
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.3hieq.mongodb.net/housing_app?retryWrites=true&w=majority`,
+{useNewUrlParser: true, useUnifiedTopology: true})
+.then(result => {
+    
+    app.listen(port, () => console.log(`Server is running on ${port}`))
+})
+.catch( err => console.log(err))
--- auth-system/server/routes/auth.js
+++ auth-system/server/routes/auth.js
@@ -1,10 +1,27 @@
 import express from "express"
+import errorValidationChecker from "../../../server/middleware/errorValidationChecker.js"
+import User from "../models/User.js"
+import userValidation from "../validator/userValidation.js"
 
 
 const router = express.Router()
 
-router.post("/register", (req, res) => {
-    res.send("register route")
+router.post("/register", [userValidation(), errorValidationChecker()], async (req, res) => {
+    try {
+    const user = new User({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: req.body.password
+    })
+    await user.save()
+
+    res.json(user)
+
+
+    } catch (err) {
+        console.log(err)
+        res.status(400).json({message: err})
+    }
 })
 
 router.post("/login", (req, res) => {
