e5e768ef813f87259035db36eead90a2c3896aa1
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-03-08 12:29:50 +0100

    register route working as it supposed to.

--- auth-system/server/routes/auth.js
+++ auth-system/server/routes/auth.js
@@ -2,25 +2,37 @@ import express from "express"
 import errorValidationChecker from "../../../server/middleware/errorValidationChecker.js"
 import User from "../models/User.js"
 import userValidation from "../validator/userValidation.js"
-
+import bcrypt from "bcryptjs"
 
 const router = express.Router()
 
 router.post("/register", [userValidation(), errorValidationChecker()], async (req, res) => {
     try {
-    const user = new User({
-        fullName: req.body.fullName,
-        email: req.body.email,
-        password: req.body.password
-    })
-    await user.save()
+        //? User exist?
 
-    res.json(user)
+        const userExist = await User.findOne({email: req.body.email})
+        if (userExist) return res.status(400).send('Email already exists')
+
+        const salt = await bcrypt.genSalt()
+        const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+        const user = new User({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            password: hashPassword
+        })
+        await user.save()
+
+        res.json({
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+        })
 
 
     } catch (err) {
         console.log(err)
-        res.status(400).json({message: err})
+        res.status(400).json({ message: err })
     }
 })
 
